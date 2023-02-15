1b4aa68f992b8dfc1c651dbee2ee02d719f81c61
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-15 18:22:56 +0200

    Routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,21 @@
+import { lazy, Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import GlobalStyle from "styles/GlobalStyle";
+import theme from "styles/theme";
+
+const FeedbackPage = lazy(() => import("../pages/feedbackPage/FeedbackPage"));
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<FeedbackPage />} />
+          <Route path="*" element={<div>Not found</div>}></Route>
+        </Routes>
+      </Suspense>
+    </ThemeProvider>
   );
 };
---
+++ src/pages/feedbackPage/FeedbackPage.jsx
@@ -0,0 +1,13 @@
+import FeedbackForm from "components/FeedbackForm/FeedbackForm";
+import Footer from "components/Footer/Footer";
+
+const FeedbackPage = () => {
+  return (
+    <div>
+      <FeedbackForm />
+      <Footer />
+    </div>
+  );
+};
+
+export default FeedbackPage;
