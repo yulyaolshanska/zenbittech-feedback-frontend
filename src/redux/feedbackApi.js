5f37d9714d26e4123091e5ede370f76fa59c81e1
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-18 14:51:10 +0200

    change server url

--- src/redux/feedbackApi.js
+++ src/redux/feedbackApi.js
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 // const BASE_URL = "http://localhost:3001/feedback";
-const BASE_URL =
-  "http://zenbittech-feedback-backend-production.up.railway.app/feedback";
-
+// const BASE_URL =
+//   "http://zenbittech-feedback-backend-production.up.railway.app/feedback";
+const BASE_URL = "https://zenbittech-feedback-backend.onrender.com";
 export const feedbackApi = createApi({
   reducerPath: "feedbackApi",
   baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   tagTypes: ["Feedbacks"],
   endpoints: (builder) => ({
     addFeedback: builder.mutation({
-      query: (data) => ({ url: "/", method: "POST", body: data }),
+      query: (data) => ({ url: "/feedback", method: "POST", body: data }),
     }),
     invalidatesTags: ["Feedbacks"],
   }),
