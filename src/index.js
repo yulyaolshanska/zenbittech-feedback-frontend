67a612d60e7b1ac5823eed1c222c451a538dba0a
Julia Olshanska <63751946+yulyaolshanska@users.noreply.github.com>
2023-02-18 16:22:48 +0200

    deploy on netlify

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="zenbittech-feedback-frontend">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
